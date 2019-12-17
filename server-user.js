481ad37a7f6173b2944c5e73820ccdd7c96e6ab3
Денис Василенко <denisvasilenko@MacBook-Pro-Denis.local>
2019-12-17 23:34:25 +0200

    user build fix

--- server-user.js
+++ server-user.js
@@ -1,9 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const proxy = require('http-proxy-middleware');
 const app = express();
-console.log(__dirname);
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/kyt-scheduler-user'));
 
@@ -15,6 +13,6 @@ app.get('/*', function(req, res) {
 //   res.send('Hello world From Server 2');
 // });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 1001, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log('Admin listening on port 1001');
 });
